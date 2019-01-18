f5e70c87e6455d5c84e3c96ac5c9540bbed8330b
TheHogNL <igor@ergens.org>
2019-01-18 21:20:52 +0100

    somes edits by terrorsource

--- update-rooted.sh
+++ update-rooted.sh
@@ -4,7 +4,7 @@ echo "==========================================================================
 echo "Welcome to the rooted Toon upgrade script. This script will try to upgrade your Toon using your original connection with Eneco. It will start the VPN if necessary."
 echo "Please be advised that running this script is at your own risk!"
 echo ""
-echo "Version: 3.44  - TheHogNL & TerrorSource & yjb - 18-1-2019"
+echo "Version: 3.45  - TheHogNL & TerrorSource & yjb - 18-1-2019"
 echo ""
 echo "==================================================================================================================================================================="
 echo ""
