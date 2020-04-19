6832a5b3b798e3d443ca51d30aaccaa4287d3cca
TheHogNL <igor@ergens.org>
2020-04-19 09:13:29 +0200

    add end notice after fixing files

--- update-rooted.sh
+++ update-rooted.sh
@@ -4,7 +4,7 @@ echo "==========================================================================
 echo "Welcome to the rooted Toon upgrade script. This script will try to upgrade your Toon using your original connection with Eneco. It will start the VPN if necessary."
 echo "Please be advised that running this script is at your own risk!"
 echo ""
-echo "Version: 4.18  - TheHogNL & TerrorSource & yjb - 24-03-2020"
+echo "Version: 4.19  - TheHogNL & TerrorSource & yjb - 19-04-2020"
 echo ""
 echo "If you like the update script for rooted toons you can support me. Any donation is welcome and helps me developing the script even more."
 echo "https://paypal.me/pools/c/8bU3eQp1Jt"
@@ -976,6 +976,7 @@ do
 			makeBackupFixFiles
 			fixFiles
 			checkFixedFiles
+			echo "Finished fixing files!"
 			exit
 			;;
 		h)      usage
